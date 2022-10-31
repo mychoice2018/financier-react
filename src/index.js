6ea69abea3620a526620177bc52f77f6f9cee4c6
vijay6390 <vijaydvk1990@gmail.com>
2022-10-31 10:21:00 +0530

    fuse theme

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React, { Suspense, lazy } from "react";
-import ReactDOM from "react-dom";
-import "./assets/scss/style.scss";
-import Spinner from "./views/spinner/Spinner";
-import "./data";
-
-// setup fake backend
-import { ConfigureFakeBackend } from "./jwt/_helpers";
-ConfigureFakeBackend();
-const App = lazy(
-  () =>
-    new Promise((resolve) => {
-      setTimeout(() => resolve(import("./app")), 0);
-    })
-);
+// By default, this project supports all modern browsers.
+// Support for Internet Explorer 11 requires polyfills.
+// For to support Internet Explorer 11, install react-app-polyfill,
+// https://github.com/facebook/create-react-app/tree/master/packages/react-app-polyfill
+// import 'react-app-polyfill/ie11';
+// import 'react-app-polyfill/stable';
+import 'typeface-muli';
+import './react-table-defaults';
+import './react-chartjs-2-defaults';
+import './styles/index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import App from 'app/App';
 
 ReactDOM.render(
-  <Suspense fallback={<Spinner />}>
-    <App />
-  </Suspense>,
-  document.getElementById("root")
+    <App/>,
+    document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
